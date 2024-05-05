806ef76c86ba15c2393ef598cc1738fd006670b7
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2024-05-05 15:26:41 -0400

    displaying address

--- MapServices/functions/map.js
+++ MapServices/functions/map.js
@@ -56,16 +56,23 @@ async function MapServices(textContent) {
             if (places.length) {
                 const { LatLngBounds } = await google.maps.importLibrary("core");
                 const bounds = new LatLngBounds();
+                const resultDiv = document.getElementById("searchResult");
+                resultDiv.innerHTML = ""; // Clear all child elements
 
                 // Loop through and get all the results.
-                places.forEach((place) => {
+                places.forEach(async (place) => {
+                    
                     const markerView = new AdvancedMarkerElement({
                         map,
                         position: place.location,
                         title: place.displayName,
                     });
-
+                    
                     bounds.extend(place.location);
+                    const address = await reverseGeocode(place.location)
+                    
+                    displayMarkerDetails(address,place.displayName,resultDiv)
+                   
                     
                 });
                 map.fitBounds(bounds);
@@ -82,3 +89,46 @@ async function MapServices(textContent) {
 
 }
 
+function displayMarkerDetails(address, title, resultDiv) {
+    // Create new paragraph elements to display position and title
+
+
+    const titleParagraph = document.createElement("h3");
+    titleParagraph.textContent =   title ;
+    const addressParagraph = document.createElement("p");
+    // Append the paragraphs to the result div
+    addressParagraph.textContent = "\nAddress:" + address;
+
+    resultDiv.appendChild(titleParagraph);
+    resultDiv.appendChild(addressParagraph);
+}
+
+
+async function reverseGeocode(latlng) {
+    // Create a geocoder object
+    const { Geocoder } = await google.maps.importLibrary("geocoding")
+    const geocoder = new Geocoder()
+  
+    try {
+      // Perform reverse geocoding
+      const response = await new Promise((resolve, reject) => {
+        geocoder.geocode({ location: latlng }, (results, status) => {
+          if (status === "OK") {
+            resolve(results);
+          } else {
+            reject(status);
+          }
+        });
+      });
+  
+      if (response && response.length > 0) {
+        // Extract the formatted address from the first result
+        const formattedAddress = response[0].formatted_address;
+        return formattedAddress;
+      } else {
+        throw new Error("No results found");
+      }
+    } catch (error) {
+      throw new Error("Geocoder failed due to: " + error);
+    }
+  }
